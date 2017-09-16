12b0b580c5e30c70a50659ceca1ec724310ca748
rhuard <ryan.huard@isilon.com>
2017-09-15 17:35:19 -0700

    opend6 finished

---
+++ app/src/main/java/com/example/rhuard/savagediceroller/OpenD6DiceEngine.java
@@ -0,0 +1,53 @@
+package com.example.rhuard.savagediceroller;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * Created by rhuard on 9/15/17.
+ */
+
+public class OpenD6DiceEngine extends DiceEngine implements DiceResuls {
+
+    public OpenD6DiceEngine(){
+        super();
+    }
+
+    public Hashtable<String, List<Integer>> HandleRoll(int num, boolean wild_die){
+        Hashtable<String, List<Integer>> results = CreateResultsTable();
+        int end = 0;
+        if(num > 0) {
+            //roll wild die if necessary
+            if (wild_die) {
+                num = num - 1;
+                int roll = 0;
+                //keep rolling if it keeps exploding
+                do{
+                    roll += RollDie(6);
+                }while(roll == 6);
+                results.put("WildDie", new ArrayList<Integer>());
+                results.get("WildDie").add(roll);
+                end += roll;
+            }
+
+            //roll rest of dice
+            int roll;
+            for(int i = 0; i < num; i++){
+                roll = RollDie(6);
+                results.get("Rolls").add(roll);
+                end += roll;
+            }
+
+            //get result
+            results.get("Final").add(end);
+        }
+        return results;
+    }
+
+    public Hashtable<String, List<Integer>> HandleRoll(int num, boolean wild_die, int modifier){
+        Hashtable<String, List<Integer>> results = HandleRoll(num, wild_die);
+        results.get("Final").set(0, results.get("Final").get(0) + modifier);
+        return results;
+    }
+}
--- app/src/main/java/com/example/rhuard/savagediceroller/OpenD6DiceRoller.java
+++ app/src/main/java/com/example/rhuard/savagediceroller/OpenD6DiceRoller.java
@@ -4,17 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.util.Set;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class OpenD6DiceRoller extends AppCompatActivity {
 
-    int num_dice;
-    boolean wild_die;
+    private int _num_dice;
+    private boolean _wild_die;
+    private OpenD6DiceEngine _dice_engine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,10 @@ public class OpenD6DiceRoller extends AppCompatActivity {
 
         setTitle("Open D6");
 
-        num_dice = 1;
-        SetNumDiceText(num_dice);
+        _dice_engine = new OpenD6DiceEngine();
+
+        _num_dice = 1;
+        SetNumDiceText(_num_dice);
 
         //Init wild die switch
         Switch mod_switch = (Switch) findViewById(R.id.wildDieSwitch);
@@ -35,30 +40,80 @@ public class OpenD6DiceRoller extends AppCompatActivity {
     public class SwitchCheckedChangedListener implements CompoundButton.OnCheckedChangeListener{
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
             if(isChecked){
-                wild_die = true;
+                _wild_die = true;
             }else{
-                wild_die = false;
+                _wild_die = false;
             }
         }
     }
 
     private void DisplayResult(String text){
-        TextView tv = (TextView) findViewById(R.id.openD6ResultView);
-        tv.setText(text);
+        TextView result_view = (TextView) findViewById(R.id.openD6ResultView);
+        TextView roll_view = (TextView) findViewById(R.id.openD6RollView);
+        TextView wild_view = (TextView) findViewById(R.id.openD6WildRollView);
+        result_view.setText(text);
+        roll_view.setText("");
+        wild_view.setText("");
+    }
+
+    private void DisplayResult(Hashtable<String, List<Integer>> result){
+        TextView result_view = (TextView) findViewById(R.id.openD6ResultView);
+        TextView roll_view = (TextView) findViewById(R.id.openD6RollView);
+        TextView wild_view = (TextView) findViewById(R.id.openD6WildRollView);
+
+        String roll_text = "";
+        if(_wild_die){
+            wild_view.setText("Wild Die: " + String.valueOf(result.get("WildDie").get(0)));
+        }else{
+            wild_view.setText("");
+        }
+        roll_text += "Rolls: " + String.valueOf(result.get("Rolls"));
+
+        roll_view.setText(roll_text);
+        result_view.setText("Final Results: " + result.get("Final").get(0));
     }
 
     //helpers
+    private String CurrentSpinnerValue() {
+        Spinner sp = (Spinner) findViewById(R.id.modifierSpinner);
+        return sp.getSelectedItem().toString();
+    }
+
     private void SetNumDiceText(int num){
         TextView tv = (TextView) findViewById(R.id.openD6numDiceView);
         tv.setText(String.valueOf(num));
     }
 
     private void UpdateDice(int change){
-        num_dice = num_dice + change;
-        if(num_dice < 0){
-            num_dice = 0;
+        _num_dice = _num_dice + change;
+        if(_num_dice < 1){
+            _num_dice = 1;
         }
-        SetNumDiceText(num_dice);
+        SetNumDiceText(_num_dice);
+    }
+
+    private void ProcessRoll(){
+        Hashtable<String, List<Integer>> result = null;
+        Switch ms = (Switch) findViewById(R.id.modifierSwitch);
+        //check if modifier is required
+        if(ms.isChecked()){
+            //create modifier
+            RadioButton mod_rd = (RadioButton) findViewById(R.id.modifierNegButton);
+            int mod = Integer.parseInt(CurrentSpinnerValue());
+            if (mod_rd.isChecked()){
+                mod = -mod;
+            }
+            result = _dice_engine.HandleRoll(_num_dice, _wild_die, mod);
+        }
+        else{
+            result = _dice_engine.HandleRoll(_num_dice, _wild_die);
+        }
+
+        if (result != null) {
+            DisplayResult(result);
+        }else{
+            DisplayResult("ERROR");
+        }
     }
 
     //Button Actions
@@ -69,5 +124,7 @@ public class OpenD6DiceRoller extends AppCompatActivity {
     public void RemoveDice(View view){
         UpdateDice(-1);
     }
+
+    public void RollDice(View view){ProcessRoll();}
 
 }
